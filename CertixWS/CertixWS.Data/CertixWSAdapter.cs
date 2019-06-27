b555556a416a493bfe7fd4dbe1754a160b32740c
VIAMATTEI\giacomo.barletti <g.barletti@topfinish.it>
2019-06-27 11:28:58 +0200

    impianto accesso al database

---
+++ CertixWS/CertixWS.Data/CertixWSAdapter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CertixWS.Data
+{
+    public class CertixWSAdapter : CertixWSAdapterBase
+    {
+        public CertixWSAdapter(System.Data.IDbConnection connection, IDbTransaction transaction) :
+            base(connection, transaction)
+        { }
+
+      
+
+
+    }
+}
--- CertixWS/CertixWS.Data/CertixWSBusiness.cs
+++ CertixWS/CertixWS.Data/CertixWSBusiness.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CertixWS.Data.Core;
 
 namespace CertixWS.Data
 {
-    public class Class1
+    public class CertixWSBusiness : CertixWSBusinessBase
     {
+        public CertixWSBusiness() : base() { }
+
+     
     }
 }
